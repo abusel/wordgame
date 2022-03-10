b67625213f0f57c0d21d6717250e736348b954e5
Andrew Busel <buselax@gmail.com>
2022-03-10 12:43:16 -0500

    new word

--- client/src/Pages/Curdle.js
+++ client/src/Pages/Curdle.js
@@ -5,7 +5,7 @@ import Guess from "../Components/guess";
 function Curdle() {
   const [guess, setGuess] = useState("");
   const [guesses, setGuesses] = useState([]);
-  let word = "board";
+  let word = "colby";
   function game(guess, secret) {
     let bulls = 0;
     let cows = 0;
